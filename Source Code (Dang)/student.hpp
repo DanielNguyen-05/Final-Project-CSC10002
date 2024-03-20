37dd2ca68bd3f2e82726974bd1a80818d8e9322b
gbao23127157 <giabao0844898907@gmail.com>
2024-03-20 16:16:15 +0700

    Update student.hpp

--- Source Code (Dang)/student.hpp
+++ Source Code (Dang)/student.hpp
@@ -3,4 +3,23 @@
 #include<string>
 
 using namespace std;
+
+struct Student {
+    string stu_id;
+    string first_name;
+    string last_name;
+    string gender;
+    string date_of_birth;
+    string soci_id;
+
+    Student() {
+        stu_id = -1;
+    }
+    Student(string stu_id, string first_name, string last_name, string gender, string date_of_birth, string soci_id);
 };
+
+ostream& operator<<(ostream& os, const Student a);
+bool operator<(const Student a, const Student b);
+bool operator>(const Student a, const Student b);
+bool operator==(const Student a, const Student b);
+bool operator!=(const Student a, const Student b);
