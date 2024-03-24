9451ee9fc5377345b37732ddef87ac4dcc4da7d5
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2024-03-24 10:24:24 +0700

    update

---
+++ SourceCode/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/Beginning_school_year.cpp.o"
+  "CMakeFiles/project.dir/Beginning_school_year.cpp.o.d"
+  "CMakeFiles/project.dir/NodeFunction.cpp.o"
+  "CMakeFiles/project.dir/NodeFunction.cpp.o.d"
+  "CMakeFiles/project.dir/User.cpp.o"
+  "CMakeFiles/project.dir/User.cpp.o.d"
+  "CMakeFiles/project.dir/consoleView.cpp.o"
+  "CMakeFiles/project.dir/consoleView.cpp.o.d"
+  "CMakeFiles/project.dir/course.cpp.o"
+  "CMakeFiles/project.dir/course.cpp.o.d"
+  "CMakeFiles/project.dir/main.cpp.o"
+  "CMakeFiles/project.dir/main.cpp.o.d"
+  "CMakeFiles/project.dir/main_menu.cpp.o"
+  "CMakeFiles/project.dir/main_menu.cpp.o.d"
+  "CMakeFiles/project.dir/schoolyear_test.cpp.o"
+  "CMakeFiles/project.dir/schoolyear_test.cpp.o.d"
+  "CMakeFiles/project.dir/semester.cpp.o"
+  "CMakeFiles/project.dir/semester.cpp.o.d"
+  "CMakeFiles/project.dir/student.cpp.o"
+  "CMakeFiles/project.dir/student.cpp.o.d"
+  "project"
+  "project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -1,14 +1,10 @@
-#include "user.hpp"
-#include "semester.hpp"
-#include "course.hpp"
-#include "student.hpp"
-#include "LinkedList.hpp"
-#include "header.hpp"
-#include "main_menu.hpp"
+#include "schoolyear_test.hpp"
 #include <iostream>
 
 using namespace std;
 
+void createSemester(); // Define the function createSemester
+
 int main() { 
-    
+    createSemester();
 }
