c1201a8c3721b101da97c27b6426ca4fdb390e72
DanielNguyen-05 <nhdang231@clc.fitus.edu.vn>
2024-03-24 19:16:01 +0700

    update

--- SourceCode/schoolyear_test.cpp
+++ SourceCode/schoolyear_test.cpp
@@ -34,7 +34,7 @@ void SchoolYear::createGeneralClass(std::string path) {
 }
 
 
-void SchoolYear::importStudentsToGeneralClassFromCSV(std::string path) {
+void SchoolYear::importStudentsToGeneralClassFromCSV(std::string path) {  // hàm này Đăng sử dụng, đừng xoá
     std::cout << "\t\t\t ADDING STUDENT TO GENERAL CLASS" << "\n\n";
     std::string year;
     std::cout << "\t -Please input the year of the School Year: ";
@@ -95,83 +95,3 @@ void Semester::createSemester() {
 
     fOut.close();
 }
-
-
-
-// void SchoolYear::createSchoolYear() {
-//     std::cout << "\t\t\t CREATING NEW SCHOOL YEAR" << "\n\n";
-//     std::string year;
-//     std::cout << "\t -Please input the year of the school year: ";
-    
-//     std::cout << "\n" << "\t -Please input path of file ypu want to save year in: ";
-//     std::string output_path;
-//     std::cin >> output_path;
-
-//     std::ofstream fOut(output_path);
-//     if (!fOut.is_open()) {
-//         std::cout << "\t -File not found!" << "\n";
-//         return;
-//     }
-//     fOut << year << ", ";
-//     fOut.close();
-// }
-
-// void SchoolYear::createGeneralClass() {
-//     std::cout << "\t\t\t CREATING A NEW GENERAL CLASS" << "\n\n";
-
-//     std::cout << "\n" << "\t -Please input the path of the file you want to save General Class in: ";
-//     std::string output_path;
-//     std::cin >> output_path;
-
-//     std::ofstream fOut(output_path);
-//     if (!fOut.is_open()) {
-//         std::cout << "\t -Failed to open the file!" << "\n";
-//         return;
-//     }
-//     std::string year;
-//     std::cout << "\t -Please input the year of the School Year: ";
-//     fOut << year;
-//     std::string class_name;
-//     std::cout << "\t -Please input the name of the new General Class: ";
-//     fOut << class_name;
-//     fOut.close();
-// }
-
-
-// void SchoolYear::importStudentsToGeneralClassFromCSV() {
-//     std::cout << "\t\t\t ADDING STUDENT TO GENERAL CLASS" << "\n\n";
-//     std::string year;
-//     std::cout << "\t -Please input the year of the School Year: ";
-//     std::cin >> year;
-//     std::string class_name;
-//     std::cout << "\t -Please input the name of the General Class: ";
-//     std::cin >> class_name;
-
-//     std::string input_path;
-//     std::cout << "\n" << "\t -Please input the path of the input file you want to get students: ";
-//     std::cin >> input_path;
-
-//     std::string output_path;
-//     std::cout << "\n" << "\t -Please input the path of the output file you want to save students in: ";
-//     std::cin >> output_path;
-
-//     std::ifstream fIn(input_path);
-//     if (!fIn.is_open()) {
-//         std::cout << "\t -File not found!" << "\n";
-//         return;
-//     }
-//     std::string ignore = "";
-//     std::getline(fIn, ignore);
-//     while (!fIn.eof()) {
-//         Student stu;
-//         std::getline(fIn, stu.stu_id, ',');
-//         std::getline(fIn, stu.first_name, ',');
-//         std::getline(fIn, stu.last_name, ',');
-//         std::getline(fIn, stu.gender, ',');
-//         std::getline(fIn, stu.date_of_birth, ',');
-//         std::getline(fIn, stu.soci_id);
-
-// 		this->students.insertOrdered(stu);
-//     }
-//     fIn.close();
-// }
--- my_sfml_project/main.cpp
+++ my_sfml_project/main.cpp
@@ -1,11 +1,29 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    // Create the SFML window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Student Management System");
 
+    // Font for text
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")) {
+        std::cerr << "Error loading font file" << std::endl;
+        return 1;
+    }
+
+    // Text for the menu options
+    sf::Text menuText;
+    menuText.setFont(font);
+    menuText.setCharacterSize(24);
+    menuText.setFillColor(sf::Color::White);
+    menuText.setString("What would you like to do?\n1. Login\n0. Stop program\n\nYour choice: ");
+
+    // Main loop
     while (window.isOpen())
     {
+        // Handle events
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -13,8 +31,13 @@ int main()
                 window.close();
         }
 
+        // Clear the window
         window.clear();
-        // here you can draw anything you want by calling window.draw(...)
+
+        // Draw the text
+        window.draw(menuText);
+
+        // Display the content of the window
         window.display();
     }
 
