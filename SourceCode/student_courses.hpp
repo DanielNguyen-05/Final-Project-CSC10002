cae88d5ff83663c977b2dd5ed39f032e4743c495
gbao23127157 <giabao0844898907@gmail.com>
2024-03-26 12:37:43 +0700

    Update student_courses.hpp

--- SourceCode/student_courses.hpp
+++ SourceCode/student_courses.hpp
@@ -1,10 +1,22 @@
 #pragma once
 #include <string>
 #include "course.hpp"
+#pragma once 
+#include<string>
 
 struct StudentCourses{
+using namespace std;
+struct StudentCourses {
+    struct Point {
+        string stu_id;
+        string full_name;
+        string overall;
+        string final;
+        string midterm;
+        string others;
 
     Course::Point point; 
+    }point;
 
     string ID;
     string course_name;
