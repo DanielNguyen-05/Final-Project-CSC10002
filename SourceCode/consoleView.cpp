f21ee747cf2bb42b649f8982bdd619275a277e38
gbao23127157 <giabao0844898907@gmail.com>
2024-03-27 20:53:57 +0700

    update consoleView

--- SourceCode/consoleView.cpp
+++ SourceCode/consoleView.cpp
@@ -62,6 +62,115 @@ void isNOTcreated()
 
                 << "Your choice: ";
 }
+void editSchoolYearMenu() {
+    std::cout   << "What would you like to do ?\n"
+                << "0. Log out\n"
+                << "1. Work on General Classes\n"
+                << "2. Work on Course Classes\n"
+                << "3. Go back to the previous page to choose another year\n"
+                << "4. Go back to the Main Menu\n"
 
+                << "Your choice: ";
+}
+void generalClassMenu() {
+    std::cout   << "What would you like to do ?\n"
+                << "0. Log out\n"
+                << "1. Create a new general class\n"
+                << "2. Edit an existed general class\n"
+                << "3. Go back to the previous page to choose another class\n"
+                << "4. Go back to the Main Menu\n"
 
+                << "Your choice: ";
+}
+void inputSchoolYearFail() {
+    std::cout   << "This school year is not existed\n"
+                << "1. Re-enter it \n"
+                << "0. Back to previous page \n\n"
 
+                << "Your choice: ";
+}
+void listSchoolYear(){
+    std::ifstream fIn;
+    fIn.open("Data//SchoolYear.txt");
+    if (!fIn.is_open()) {
+        std::cerr << "error open SchoolYear.txt";
+    }
+    std::string year;
+    int no = 0;
+    while (!fIn.eof()) {
+        fIn >> year;
+        no++;
+        std::cout << no << ". " << year << std::endl;
+    }
+    fIn.close();
+    
+}
+void view_list_of_general_class(std::string year)
+{
+    std::ifstream fin;
+    std::string s;
+    std::string path = "Data\\" + year + "\\General_class.txt";
+    fin.open(path);
+    if (!fin.is_open())
+    {
+        std::cout << "Can't open file. ";
+        return;
+    }
+    while (!fin.eof())
+    {
+        fin >> s;
+        std::cout << s << "\n";
+    }
+    fin.close();
+}
+void inputClassFail() {
+    std::cout << "This class is not existed\n"
+        << "1. Re-enter it \n"
+        << "0. Back to previous page \n\n"
+
+        << "Your choice: ";
+}
+void editGeneralClassMenu() {
+    std::cout << "What would you like to do ?\n"
+        << "0. Log out\n"
+        << "1. Update the information of the general class\n"
+        << "2. View list of students in the class\n"
+        << "3. Import the list of students from .csv file into the general class\n"
+        << "4. Add a student to class\n"
+        << "5. Edit information of student\n"
+        << "6. Go back to the previous page to choose another class\n"
+        << "7. Go back to the Main Menu\n"
+
+        << "Your choice: ";
+}
+void createSchoolYearFail() {
+    std::cout << "This school year is existed\n"
+        << "1. Re-enter it \n"
+        << "0. Back to previous page \n\n"
+
+        << "Your choice: ";
+}
+void semesterMainMenu() {
+    std::cout << "What would you like to do ?\n"
+        << "0. Log out\n"
+        << "1. Create a new semesters\n"
+        << "2. Edit an existed semesters\n"
+        << "3. Go back to the previous page\n"
+        << "4. Go back to the Main Menu\n"
+
+        << "Your choice: ";
+}
+void inputSemesterFail() {
+    std::cout << "This semester is not existed\n"
+        << "1. Re-enter it \n"
+        << "0. Back to previous page \n\n"
+
+        << "Your choice: ";
+}
+void createSemesterFail() {
+    std::cout << "This semester is existed\n"
+        << "1. Re-enter it \n"
+        << "0. Back to previous page \n\n"
+
+        << "Your choice: ";
+}
---
+++ SourceCode/consoleView.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+void programInterface();
+void chooseRole();
+void loginFail();
+void staffMainMenu(std::string username);
+void changePasswordFail();
+void createNewSchoolYear();
+void isNOTcreated();
+void editSchoolYearMenu();
+void generalClassMenu();
+void inputSchoolYearFail();
+void listSchoolYear();
+void view_list_of_general_class(std::string year);
+void inputClassFail();
+void editGeneralClassMenu();
+void createSchoolYearFail();
+void semesterMainMenu();
+void inputSemesterFail();
+void createSemesterFail();
+
