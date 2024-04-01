b90aa60cb98d18c444f66d16041addf827a966a2
gbao23127157 <giabao0844898907@gmail.com>
2024-04-01 20:20:32 +0700

    update

--- SourceCode/student.hpp
+++ SourceCode/student.hpp
@@ -1,12 +1,10 @@
 #pragma once
-#include "LinkedList.hpp"
-#include"student_courses.hpp"
 #include<iostream>
 #include<string>
 
 using namespace std;
 
-struct Student{
+struct Student {
     string stu_id;
     string first_name;
     string last_name;
@@ -14,14 +12,12 @@ struct Student{
     string date_of_birth;
     string soci_id;
 
-    LinkedList<StudentCourses> stu_courses;
-
     Student() {
         stu_id = -1;
     }
     Student(string stu_id, string first_name, string last_name, string gender, string date_of_birth, string soci_id);
-
 };
+
 ostream& operator<<(ostream& os, const Student a);
 bool operator<(const Student a, const Student b);
 bool operator>(const Student a, const Student b);
