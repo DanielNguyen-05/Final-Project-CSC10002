c763be993443f9e7e61ba60ef2b47cfaee7df966
DanielNguyen-05 <161058953+DanielNguyen-05@users.noreply.github.com>
2024-04-20 00:01:29 +0700

    Add files via upload

---
+++ ui_mainpage.h
@@ -0,0 +1,72 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainpage.ui'
+**
+** Created by: Qt User Interface Compiler version 6.6.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINPAGE_H
+#define UI_MAINPAGE_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAbstractButton>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QLabel>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Dialog
+{
+public:
+    QDialogButtonBox *buttonBox;
+    QLabel *label;
+    QLabel *label_2;
+
+    void setupUi(QDialog *Dialog)
+    {
+        if (Dialog->objectName().isEmpty())
+            Dialog->setObjectName("Dialog");
+        Dialog->resize(568, 615);
+        buttonBox = new QDialogButtonBox(Dialog);
+        buttonBox->setObjectName("buttonBox");
+        buttonBox->setGeometry(QRect(30, 240, 341, 32));
+        buttonBox->setOrientation(Qt::Horizontal);
+        buttonBox->setStandardButtons(QDialogButtonBox::Cancel|QDialogButtonBox::Ok);
+        label = new QLabel(Dialog);
+        label->setObjectName("label");
+        label->setGeometry(QRect(80, 20, 414, 547));
+        label->setStyleSheet(QString::fromUtf8("background-image: url(:/background/background_login.png);\n"
+"border-radius: 20px;"));
+        label_2 = new QLabel(Dialog);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(100, 50, 371, 501));
+        label_2->setMouseTracking(false);
+        label_2->setStyleSheet(QString::fromUtf8("background-color: rgba(0, 0, 0, 139);\n"
+"border-radius:15px;"));
+
+        retranslateUi(Dialog);
+        QObject::connect(buttonBox, &QDialogButtonBox::accepted, Dialog, qOverload<>(&QDialog::accept));
+        QObject::connect(buttonBox, &QDialogButtonBox::rejected, Dialog, qOverload<>(&QDialog::reject));
+
+        QMetaObject::connectSlotsByName(Dialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *Dialog)
+    {
+        Dialog->setWindowTitle(QCoreApplication::translate("Dialog", "Dialog", nullptr));
+        label->setText(QString());
+        label_2->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Dialog: public Ui_Dialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINPAGE_H
